52b658195a0eda37feb4c7dca7475fe67a33dbe5
Ryan Dabir <ryandabir@gmail.com>
2018-06-04 20:46:34 -0400

    added two more questions and solutions

--- src/Problems.java
+++ src/Problems.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -136,6 +137,91 @@ public class Problems {
         return true;
     }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        Stack<Integer> num1 = new Stack<>();
+        Stack<Integer> num2 = new Stack<>();
+        int count1 = 0;
+        int count2 = 0;
+
+        while (l1 != null) {
+            num1.push(l1.val);
+            l1 = l1.next;
+            count1++;
+        }
+
+        while (l2 != null) {
+            num2.push(l2.val);
+            l2 = l2.next;
+            count2++;
+        }
+
+        Integer[] array1 = new Integer[count1];
+        Integer[] array2 = new Integer[count2];
+
+        for(int i = 0; i < count1; i++) {
+            array1[i] = num1.pop();
+        }
+
+        for(int i = 0; i < count2; i++) {
+            array2[i] = num2.pop();
+        }
+
+        StringBuilder str1 = new StringBuilder();
+        for (int num : array1) {
+            str1.append(num);
+        }
+        int final1 = Integer.parseInt(str1.toString());
+
+        StringBuilder str2 = new StringBuilder();
+        for (int num : array2) {
+            str2.append(num);
+        }
+        int final2 = Integer.parseInt(str2.toString());
+
+        int answer = final1 + final2;
+
+        String answerString = Integer.toString(answer);
+        int[] answerArray = new int[answerString.length()];
+        for (int i = 0; i < answerString.length(); i++) {
+            answerArray[i] = answerString.charAt(i) - '0';
+        }
+
+        return null;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        HashSet<Character> set = new HashSet<>();
+        int length = 0;
+        int answer = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if (length > answer) {
+                answer = length;
+            }
+
+            length = 1;
+            set.clear();
+            set.add(s.charAt(i));
+            for(int j = i + 1; j < s.length(); j++) {
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }
+                else {
+                    set.add(s.charAt(j));
+                    length++;
+                }
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
@@ -198,6 +284,32 @@ public class Problems {
 
             int testInt = -121;
             System.out.println(isPalindrome(testInt));
+        }
+
+        if (num == 6) {
+            //https://leetcode.com/problems/add-two-numbers/description/
+            //uncompleted
+
+            ListNode a = new ListNode(2);
+            ListNode b = new ListNode(4);
+            a.next = b;
+            ListNode c = new ListNode(3);
+            b.next = c;
+
+            ListNode d = new ListNode(5);
+            ListNode e = new ListNode(6);
+            d.next = e;
+            ListNode f = new ListNode(4);
+            e.next = f;
+
+            addTwoNumbers(a, d);
+        }
+
+        if (num == 7) {
+            //https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+            String testString = "";
+            System.out.println(lengthOfLongestSubstring(testString));
         }
     }
 }
